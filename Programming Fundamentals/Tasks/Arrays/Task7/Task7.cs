b949cda5173bd8721641f8a58b745a2b07ab9a68
SRDim <stilrdim@gmail.com>
2019-05-29 15:10:18 +0100

    Added instructions

--- Programming Fundamentals/Tasks/Arrays/Task7/Task7.cs
+++ Programming Fundamentals/Tasks/Arrays/Task7/Task7.cs
@@ -45,3 +45,9 @@ namespace Arrays
         }
     }
 }
+/*
+⦁	Equal Arrays
+Read two arrays and print on the console whether they are identical or not. Arrays are identical if their elements are equal. 
+If the arrays are identical find the sum of the first one and print on the console following message: "Arrays are identical. Sum: {sum}", 
+otherwise find the first index where the arrays differ and print on the console following message: "Arrays are not identical. Found difference at {index} index".
+*/
