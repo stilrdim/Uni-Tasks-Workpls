95bcc14c101323504eed08b2269328e2d0ea2ee5
SRDim <stilrdim@gmail.com>
2019-05-29 14:52:03 +0100

    Task 6 Initial Commit

--- Programming Fundamentals/Tasks/Arrays/Task6/Task6.cs
+++ Programming Fundamentals/Tasks/Arrays/Task6/Task6.cs
@@ -4,11 +4,65 @@ using System.Linq;
 
 namespace Arrays
 {
-    class Program
+    class Task6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+
+            // An array from the input, separating each element by the space
+            string[] inputToArray = input.Split(" ");
+
+            // An array for storing the even numbers
+            int[] evenNumbers = new int[inputToArray.Length];
+
+            // An array for storing the odd numbers
+            int[] oddNumbers = new int[inputToArray.Length];
+
+            // Hold the value of the total sum of the even numbers array
+            int totalSumEven = 0;
+
+            // Hold the value of the total sum of the even numbers array
+            int totalSumOdd = 0;
+
+            // Go over each element from the input
+            for (int i = 0; i < inputToArray.Length; i++)
+            {
+                // Convert the elements from String to Int32
+                int numberToInt32 = int.Parse(inputToArray[i]);
+
+                // If the current number is even, add it to the evenNumbers array
+                if (numberToInt32 % 2 == 0)
+                {
+                    evenNumbers[i] = numberToInt32;
+                }
+                else
+                {
+                    oddNumbers[i] = numberToInt32;
+                }
+            }
+
+            // The total sum of all even numbers
+            foreach (int number in evenNumbers)
+            {
+                totalSumEven += number;
+            }
+
+            // The total sum of all odd numbers
+            foreach (int number in oddNumbers)
+            {
+                totalSumOdd += number;
+            }
+
+            // Store the difference
+            int difference = totalSumEven - totalSumOdd;
+
+            Console.WriteLine(difference);
         }
     }
 }
+
+/*
+ ⦁	Even and Odd Subtraction
+Write a program that calculates the difference between the sum of the even and the sum of the odd numbers in an array.
+*/
