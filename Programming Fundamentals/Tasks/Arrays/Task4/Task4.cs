be5b153e7c9aa59084e414db204544ba84dfd41b
SRDim <stilrdim@gmail.com>
2019-05-29 14:41:29 +0100

    Task 4 Initial commit

---
+++ Programming Fundamentals/Tasks/Arrays/Task4/Task4.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Arrays
+{
+    class Task4
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            // Create an array from the input, separating each element by the space
+            string[] inputToArray = input.Split(" ");
+
+            // Reverse the elements in the array
+            Array.Reverse(inputToArray, 0, inputToArray.Length);
+
+            Console.WriteLine(string.Join(" ", inputToArray));
+        }
+    }
+}
