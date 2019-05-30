5e633bf37139495e2becf3c24e6327ff4cbb1146
SRDim <stilrdim@gmail.com>
2019-05-30 05:06:35 +0100

    Data Types Task 2 Initial Commit

---
+++ Programming Fundamentals/Tasks/DT-M/DT Task2/DTTask2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+
+namespace DataTypes
+{
+    class DTTask2
+    {
+        static void Main(string[] args)
+        {
+            // Ask for amount of numbers to print
+            int arraySize = int.Parse(Console.ReadLine());
+
+            // An array with the pair of numbers
+            long[] numbersArray = new long[2];
+
+            // An array with the current pair
+            string[] currentInput = new string[arraySize];
+
+
+            // Ask for input until the desired amount of numbers is reached
+            for (int i = 0; i < arraySize; i++)
+            {
+                // Prompt for the two numbers
+                currentInput[i] = Console.ReadLine();
+
+                // Convert the two numbers to Int64 (long)
+                numbersArray[0] = Convert.ToInt64(currentInput[i].Split(" ")[0]);
+                numbersArray[1] = Convert.ToInt64(currentInput[i].Split(" ")[1]);
+
+                // An array with each digit of the left number, 18 is the max size (in chars) of a long number
+                int[] leftSideDigits = new int[18];
+                int leftSideSum = 0;
+
+                // An array with each digit of the right number, 18 is the max size (in chars) of a long number
+                int[] rightSideDigits = new int[18];
+                int rightSideSum = 0;
+
+                if (numbersArray[0] > numbersArray[1])
+                {
+                    for (int j = 0; j < numbersArray[0].ToString().Length; j++)
+                    {
+                        // Get each digit by converting the number to string and looping over it (current loop)
+                        string leftNumberString = numbersArray[0].ToString();
+
+                        // Add the current number to the Int32 array after converting it to string to avoid getting the ASCII values
+                        leftSideDigits[j] = Convert.ToInt32(leftNumberString[j].ToString());
+
+                        // Add the current number to the total sum of the left side
+                        leftSideSum += leftSideDigits[j];
+                    }
+                    Console.WriteLine(leftSideSum);
+                }
+                else
+                {
+                    for (int j = 0; j < numbersArray[1].ToString().Length; j++)
+                    {
+                        string rightNumberString = numbersArray[1].ToString();
+                        rightSideDigits[j] = Convert.ToInt32(rightNumberString[j].ToString());
+                        rightSideSum += rightSideDigits[j];
+                    }
+                    Console.WriteLine(rightSideSum);
+                }
+            }
+        }
+    }
+}
+// TODO: Fix index out of range at line 28
